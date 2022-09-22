97719c7660f5846301deba84bf34721472c2217c
SkyRavenS1lver <101544440+SkyRavenS1lver@users.noreply.github.com>
2022-09-22 21:02:30 +0700

    Second Commit

---
+++ app/src/main/java/com/example/tugas/MasukFragment.java
@@ -0,0 +1,26 @@
+package com.example.tugas;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MasukFragment extends Fragment {
+    public MasukFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_masuk, container, false);
+    }
+}
