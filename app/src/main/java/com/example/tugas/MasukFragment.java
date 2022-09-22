087fd1ee0753424f56397f1d34b958cb46df4e12
SkyRavenS1lver <101544440+SkyRavenS1lver@users.noreply.github.com>
2022-09-22 21:12:03 +0700

    Second Commit

--- app/src/main/java/com/example/tugas/MasukFragment.java
+++ app/src/main/java/com/example/tugas/MasukFragment.java
@@ -1,5 +1,6 @@
 package com.example.tugas;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,20 +8,33 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class MasukFragment extends Fragment {
+    Button btnMasuk;
     public MasukFragment() {
         // Required empty public constructor
     }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_masuk, container, false);
+        btnMasuk = (Button) view.findViewById(R.id.buttonMasuk);
+        btnMasuk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(),MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_masuk, container, false);
+        return view;
     }
 }
